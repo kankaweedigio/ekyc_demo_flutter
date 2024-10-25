23598276226ad987187eda68e2c28750f86a0f29
Kankawee <kankawee@digio.co.th>
2024-10-25 17:55:46 +0700

    Add Ekyc lib & implement in iOS

---
+++ ios/EkycFramework.xcframework/ios-arm64_x86_64-simulator/EkycFramework.framework/Headers/EkycFramework.h
@@ -0,0 +1,18 @@
+//
+//  EkycFramework.h
+//  EkycFramework
+//
+//  Created by PATHOMPHONG CHAROENWICHIANCHAY on 7/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EkycFramework.
+FOUNDATION_EXPORT double EkycFrameworkVersionNumber;
+
+//! Project version string for EkycFramework.
+FOUNDATION_EXPORT const unsigned char EkycFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EkycFramework/PublicHeader.h>
+
+
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,71 @@
 import Flutter
 import UIKit
+import EkycFramework
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+    var env: String = "UAT"
+    var sessionId: String = NSUUID().uuidString.lowercased()
+    var nonProdToken: String = "Token"
+    
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let channel = FlutterMethodChannel(name: "com.test.ekyc/initEkyc", binaryMessenger: controller.binaryMessenger)
+      
+      
+      channel.setMethodCallHandler{ (call:FlutterMethodCall,result:@escaping FlutterResult) in
+          if call.method == "initEkyc" {
+              print(call.method)
+              EkycFrameworkKit.initEkyc(sessionId: self.sessionId,
+                                        token: "",
+                                        environment: self.env,
+                                        customizeTheme: nil,
+                                        language: nil){
+                  success,description,ekycToken in
+                  DispatchQueue.main.async {
+                                          guard let rootVc = UIApplication.shared.keyWindow?.rootViewController else {
+                                              print("No root view controller found")
+                                              return
+                                          }
+                                          
+                      self.livenessCheck(rootVc: rootVc, token: self.nonProdToken)
+                      
+                                      }
+                  
+                  
+              }
+              
+          }
+      }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
+    
+    private func configToken() -> String{
+        return nonProdToken
+    }
+    
+    private func livenessCheck(rootVc: UIViewController,token: String?){
+        print("livenessCheck")
+        let ekycToken = token ?? "(null)"
+        print("token: "+ekycToken)
+        EkycFrameworkKit.livenessCheck(fromViewController: rootVc){
+            success,description in
+            let data = "success:" + String(success)
+            + "\ndescription:" + description
+            + "\nekycToken:" + ekycToken + "\n"
+            print(data)
+        }
+        
+    }
+    
+    
+       
+    }
+
